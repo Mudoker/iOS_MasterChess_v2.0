6e35d3fea92db5e296a7d70814c9b13e8d54186c
Mudoker <95454901+Mudoker@users.noreply.github.com>
2023-08-17 19:13:57 +0700

    has bug in GameView

--- Master Chess/Components/Screens/Game View/GameView.swift
+++ Master Chess/Components/Screens/Game View/GameView.swift
@@ -8,24 +8,23 @@
 import SwiftUI
 
 struct GameView: View {
-    @State var currentUser: Users = Users()
+    @State private var currentUser: Users = Users()
     @State private var currentPiece: (Piece?, CGSize) = (nil, .zero)
     @ObservedObject var viewModel: GameViewModel
-    @State var isRotatingWhite = true
+    @State private var isRotatingWhite = true
 
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Users.username, ascending: true)], animation: .default) private var users: FetchedResults<Users>
-    @State var isRotatingWhite = true
+    @AppStorage("userName") private var username = "Mudoker"
 
-    @AppStorage("userName") var username = "Mudoker"
-    
     init(user: Users) {
-            viewModel = GameViewModel(user: user)
+        viewModel = GameViewModel(user: user)
     }
+
     func getUserWithUsername(_ username: String) -> Users? {
         return users.first { $0.username == username }
     }
-    var board = ChessBoard().createInitialBoard()
+
     var body: some View {
         ZStack {
             ChessBoardView()
@@ -33,21 +32,24 @@ struct GameView: View {
                 Image(piece.imageName)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .scaleEffect(viewModel.currentPiece.0 == piece ? 1.3 : 1)
-                    .offset(viewModel.currentPiece.0 == piece ? viewModel.currentPiece.1 : viewModel.indexOf(piece).size)
+                    .scaleEffect(currentPiece.0 == piece ? 1.3 : 1)
+                    .offset(currentPiece.0 == piece ? currentPiece.1 : viewModel.indexOf(piece).size)
                     .frame(width: UIScreen.main.bounds.width / 8, height: UIScreen.main.bounds.width / 8)
                     .gesture(viewModel.dragGesture(piece))
                     .animation(.easeInOut(duration: 0.2))
             }
         }
         .onAppear {
-            currentUser = getUserWithUsername(username) ?? Users()
+            if let user = getUserWithUsername(username) {
+                currentUser = user
+                viewModel = GameViewModel(user: user)
+            }
         }
     }
 }
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        GameView(viewModel: GameViewModel()) // Provide an instance of your GameViewModel here
+        GameView(user: Users())
     }
 }
