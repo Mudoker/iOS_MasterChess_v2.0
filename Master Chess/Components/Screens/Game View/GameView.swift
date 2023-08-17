4975633bf0044851bbbbb5414be2234b522bcc97
Quoc Doan Huu <s3927776@iMac-92.RMIT.EDU.VN>
2023-08-17 19:40:08 +0700

    file reallocation

--- Master Chess/Components/Screens/Game View/GameView.swift
+++ Master Chess/Components/Screens/Game View/GameView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct GameView: View {
     @State private var currentUser: Users = Users()
     @State private var currentPiece: (ChessPiece?, CGSize) = (nil, .zero)
-    @ObservedObject var viewModel: GameViewModel
+//    @ObservedObject var viewModel: GameViewModel
     @State private var isRotatingWhite = true
 
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Users.username, ascending: true)], animation: .default) private var users: FetchedResults<Users>
     @AppStorage("userName") private var username = "Mudoker"
 
-    init(user: Users) {
-        viewModel = GameViewModel(user: user)
-    }
+//    init(user: Users) {
+//        viewModel = GameViewModel(user: user)
+//    }
 
     func getUserWithUsername(_ username: String) -> Users? {
         return users.first { $0.username == username }
@@ -28,42 +28,36 @@ struct GameView: View {
     var body: some View {
         ZStack {
             ChessBoardView()
-            ForEach(viewModel.pieces) { piece in
-                Image(piece.imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaleEffect(currentPiece.0 == piece ? 1.3 : 1)
-                    .offset(currentPiece.0 == piece ? currentPiece.1 : viewModel.indexOf(piece).size)
-                    .frame(width: UIScreen.main.bounds.width / 8, height: UIScreen.main.bounds.width / 8)
-                    .gesture(viewModel.dragGesture(piece))
-                    .animation(.easeInOut(duration: 0.2))
-            }
-        }
-        .onAppear {
-            if let user = getUserWithUsername(username) {
-                currentUser = user
-                viewModel = GameViewModel(user: user)
-            }
+//            ForEach(viewModel.pieces) { piece in
+//                Image(piece.imageName)
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .scaleEffect(currentPiece.0 == piece ? 1.3 : 1)
+//                    .offset(currentPiece.0 == piece ? currentPiece.1 : viewModel.indexOf(piece).size)
+//                    .frame(width: UIScreen.main.bounds.width / 8, height: UIScreen.main.bounds.width / 8)
+//                    .gesture(viewModel.dragGesture(piece))
+//                    .animation(.easeInOut(duration: 0.2))
+//            }
         }
     }
 
-    private func dragGesture(_ piece: ChessPiece) -> _EndedGesture<_ChangedGesture<DragGesture>> {
-        DragGesture()
-            .onChanged { dragValue in
-                self.currentPiece = (piece, self.viewModel.indexOf(piece).size + dragValue.translation)
-                self.viewModel.objectWillChange.send()
-        }
-        .onEnded {
-            self.currentPiece = (nil, .zero)
-            let finalPosition = self.viewModel.indexOf(piece) + Position($0.translation)
-            let move = Move(start: self.viewModel.indexOf(piece), end: finalPosition)
-            self.viewModel.didMove(move: move)
-        }
-    }
+//    private func dragGesture(_ piece: ChessPiece) -> _EndedGesture<_ChangedGesture<DragGesture>> {
+//        DragGesture()
+//            .onChanged { dragValue in
+//                self.currentPiece = (piece, self.viewModel.indexOf(piece).size + dragValue.translation)
+//                self.viewModel.objectWillChange.send()
+//        }
+//        .onEnded {
+//            self.currentPiece = (nil, .zero)
+//            let finalPosition = self.viewModel.indexOf(piece) + Position($0.translation)
+//            let move = Move(start: self.viewModel.indexOf(piece), end: finalPosition)
+//            self.viewModel.didMove(move: move)
+//        }
+//    }
 }
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        GameView(user: Users())
+        GameView()
     }
 }
