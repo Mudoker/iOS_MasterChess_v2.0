8196cf723c617f54a8611aa7050d4663de8e11a6
Mudoker <95454901+Mudoker@users.noreply.github.com>
2023-09-08 12:54:03 +0700

    ok

---
+++ Desktop/Prismm/Prismm/PrismmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismmApp.swift
+//  Prismm
+//
+//  Created by Quoc Doan Huu on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PrismmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
